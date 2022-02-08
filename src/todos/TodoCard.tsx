dac6314297f998a63570de5e6575cf0d6ddbe5a0
David Seija <davidseija@gmail.com>
2022-02-08 18:23:11 -0500

    💄 Updated TodoCard styles

--- src/todos/TodoCard.tsx
+++ src/todos/TodoCard.tsx
@@ -4,16 +4,11 @@ function TodoCard({ data }: { data: ITodo }) {
   return (
     <article className="bg-white rounded shadow p-4 max-h-80 border border-white hover:shadow-xl hover:border-gray-400">
       <h3 className="mb-6">{data.title}</h3>
-      <p>
-        <span
-          className={`rounded-lg py-1 px-2 border text-xs ${
-            data.completed
-              ? 'border-green-500 text-green-500'
-              : 'border-yellow-500 text-yellow-500'
-          }`}
-        >
-          {data.completed ? 'Completed' : 'Pending'}
+      <p className="text-xs font-light text-gray-400">
+        <span className={data.completed ? 'text-green-500' : 'text-yellow-500'}>
+          &#9679;
         </span>
+        <span className="ml-2">{data.completed ? 'COMPLETED' : 'PENDING'}</span>
       </p>
     </article>
   );
